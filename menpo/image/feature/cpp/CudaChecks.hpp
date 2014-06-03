7944e57a0995556764ec99dc697f98a889daf1af
Nicolas DUBIEN <github@dubien.org>
2014-06-03 15:08:16 +0100

    Typos in #ifndef of CudaChecks.hpp

--- menpo/image/feature/cpp/CudaChecks.hpp
+++ menpo/image/feature/cpp/CudaChecks.hpp
@@ -1,5 +1,5 @@
-#ifndef __ERRORS_CUH__
-#define __ERRORS_CUH__
+#ifndef __CUDACHECKS_HPP__
+#define __CUDACHECKS_HPP__
 
 #define cudaErrorRaise(err) { fprintf(stderr, "Cuda error in file '%s' in line %i : %s.\n", __FILE__, __LINE__, cudaGetErrorString(err)); exit(1); }
 #define cudaErrorCheck(call) { cudaAssert(call,__FILE__,__LINE__); }
